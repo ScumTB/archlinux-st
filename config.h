1e6b3c854a951ca4c0612cfbfd4b15e886902ff2
ScumTB <981775064@qq.com>
2023-05-04 22:15:36 +0800

    new commit

--- config.h
+++ config.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.82;//值越大越不透明
+float alpha = 0.85;//值越大越不透明
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
